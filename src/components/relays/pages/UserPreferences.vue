b0031a028ac802ab327601eb14fdaea443b2b185
dskvr <dskvr@users.noreply.github.com>
2023-01-30 02:51:15 +0100

    add descriptions

--- src/components/relays/pages/UserPreferences.vue
+++ src/components/relays/pages/UserPreferences.vue
@@ -37,7 +37,7 @@
                   :class="store.prefs.refresh ? 'bg-indigo-600' : 'bg-gray-200 dark:bg-black'"
                   class="relative inline-flex h-5 w-10 shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75"
                 >
-                  <span class="sr-only">Use setting</span>
+                  <span class="sr-only">Automatically refresh?</span>
                   <span
                     aria-hidden="true"
                     :class="store.prefs.refresh ? 'translate-x-5' : 'translate-x-0'"
@@ -65,7 +65,7 @@
                     class="pointer-events-none inline-block h-4 w-4 transform rounded-full bg-white shadow-lg ring-0 transition duration-200 ease-in-out"
                   />
                 </Switch>
-                <p class="mt-2 text-sm text-gray-500">Automatically refresh</p>
+                <p class="mt-2 text-sm text-gray-500">Pin favorites to top</p>
               </div>
             </div>
 
